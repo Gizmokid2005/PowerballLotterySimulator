3e15c9f7b290c3bf4b0d3279a727f4f79afaf7c0
Tim Nguyen <timnguyen@Tims-MBP.fios-router.home>
2016-01-11 22:00:03 -0500

    Fix args parser

--- arg_parser.rb
+++ arg_parser.rb
@@ -21,9 +21,9 @@ class ArgParser
       o.banner = "Usage: Simulate Powerball Lottery"
       o.separator  ""
       o.separator  "Options"
-      o.on(     '-N', "--number_of_simulated games=number", Integer, "Provide number of games you want simulated.") { |number| @options[:number_of_games] =  number}
-      o.on(     '-B', "--number_white_balls_match=balls", Integer, "Provide number of white balls match to consider a win.") { |balls| @options[:number_white_balls_match] = balls }
-      o.on(     '-R', "--red_ball_match", "Flag for whether red ball is matched to win.") { @options[:match_red_ball] = true}
+      o.on(     '-N', "--simulated_games  number_of_simulated_games", Integer, "Provide number of games you want simulated.") { |number| @options[:number_of_games] =  number}
+      o.on(     '-B', "--white_balls_match  number_of_white_balls_match", Integer, "Provide number of white balls match to consider a win.") { |balls| @options[:number_white_balls_match] = balls }
+      o.on(     '-R', "--red_ball_match", "Flag for whether red ball should be matched to consider a win.") { @options[:match_red_ball] = true}
       o.on_tail('-H', "--help"               , "Show this message") { puts o; exit }
     end
   end
@@ -36,7 +36,7 @@ class ArgParser
   end
 
   def all_required_values_entered?
-    return !@options[:number_of_games].nil? || !@options[:number_white_balls_match].nil?
+    return !@options[:number_of_games].nil? && !@options[:number_white_balls_match].nil?
   end
 
   def get_error_message_for_missing_required_argument
